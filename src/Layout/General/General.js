0e5d2203b1503c509e41e77a6af816d7437bf1bc
박나리 <>
2021-10-07 17:41:34 +0900

    feature: context api 를 사용하여 toggleTheme 함수를 하위 컴포넌트들에서 사용할 수 있도록 구현 #3

--- src/Layout/General/General.js
+++ src/Layout/General/General.js
@@ -1,10 +1,15 @@
 import { Layout } from 'antd';
+import { useContext } from 'react';
+import { ThemeContext } from '../../RootTheme';
 
 const General = ({ children }) => {
   const { Header, Footer, Content } = Layout;
+  // fixme: toggle theme 사용 예제
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  console.log(theme);
   return (
     <Layout>
-      <Header>헤더</Header>
+      <Header onClick={() => toggleTheme(theme)}>헤더</Header>
       <Content>{children}</Content>
       <Footer>Footer</Footer>
     </Layout>
--- src/RootTheme.js
+++ src/RootTheme.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import { light, dark } from './styles';
 import { ThemeProvider } from 'styled-components';
 
+export const ThemeContext = createContext({});
 const RootTheme = ({ children }) => {
   const [theme, setTheme] = useState(true);
+  const themeMode = theme ? light : dark;
+  const toggleTheme = mode => setTheme(!mode);
 
-  return <ThemeProvider theme={theme ? light : dark}>{children}</ThemeProvider>;
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ThemeProvider theme={themeMode}>{children}</ThemeProvider>
+    </ThemeContext.Provider>
+  );
 };
 
 export default RootTheme;
